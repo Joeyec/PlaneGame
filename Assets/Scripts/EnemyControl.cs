4b1325237101a743284a924e6edc8a9e22c167d2
Joeyec <1329097376@qq.com>
2019-09-19 11:08:16 +0800

    push Plane

---
+++ Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour {
+    public float speed = 2f;
+    public GameObject ExplosionGO;
+    GameObject scoreUITextGO;
+	// Use this for initialization
+	void Start () {
+        scoreUITextGO = GameObject.FindGameObjectWithTag("ScoreTextTag");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 position = transform.position;
+        position = new Vector2(position.x, position.y - speed * Time.deltaTime);
+        transform.position = position;
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        if (transform.position.y < min.y)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if ((col.tag == "PlayerShipTag") || (col.tag == "PlayerBulletTag"))
+        {
+            PlayExplosion();
+            scoreUITextGO.GetComponent<GameScore>().Score += 100;
+            Destroy(gameObject);
+        }
+    }
+    void PlayExplosion()
+    {
+        GameObject explosion = Instantiate(ExplosionGO);
+        explosion.transform.position = transform.position;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playButton;
+    public GameObject playerShip;
+    public GameObject enemySpawner;
+    public GameObject GameOverGO;
+    public GameObject scoreUITextGo;
+    public GameObject TimeCounterGo;
+    public enum GameManagerState
+    {
+        Opening,
+        GamePlay,
+        GameOver,
+    }
+    GameManagerState GMState;
+    // Use this for initialization
+    void Start()
+    {
+        GMState = GameManagerState.Opening;
+    }
+    void UpdateGameManagerState()
+    {
+        switch (GMState)
+        {
+            case GameManagerState.Opening:
+                GameOverGO.SetActive(false);
+                playButton.SetActive(true);
+                //playerShip.GetComponent<PlayerControl>().Init();
+                //playerShip.SetActive(false);
+                break;
+            case GameManagerState.GamePlay:
+                scoreUITextGo.GetComponent<GameScore>().Score = 0;
+                playButton.SetActive(false);
+                playerShip.GetComponent<PlayerControl>().Init();
+                enemySpawner.GetComponent<EnemySpawner>().ScheduleEnemySpawner();
+                TimeCounterGo.GetComponent<TimeCounter>().StartTimeCounter();
+                break;
+            case GameManagerState.GameOver:
+                TimeCounterGo.GetComponent<TimeCounter>().StopTimeCounter();
+                enemySpawner.GetComponent<EnemySpawner>().UnscheduleEnemySpawner();
+                GameOverGO.SetActive(true);
+                Invoke("ChangeToOpeningState", 8f);
+                break;
+        }
+    }
+    public void SetGameManagerState(GameManagerState state)
+    {
+        GMState = state;
+        UpdateGameManagerState();
+    }
+    public void StartGamePlay()
+    {
+        GMState = GameManagerState.GamePlay;
+        UpdateGameManagerState();
+    }
+    public void ChangeToOpeningState()
+    {
+        SetGameManagerState(GameManagerState.Opening);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
